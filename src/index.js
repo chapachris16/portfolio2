a0e1c088eb8d338ed53ba72102abea23cbf545ac
christopher chapa <chapachristopher16@gmail.com>
2023-01-09 19:32:16 -0600

    fixing routes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='https://chapachris16.github.io/portfolio2'>
       <App />
     </Router>
   </React.StrictMode>
