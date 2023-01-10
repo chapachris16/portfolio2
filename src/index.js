a035ef30c2d39b6045e85f6a859e3d21a9b42f26
christopher chapa <chapachristopher16@gmail.com>
2023-01-09 19:38:20 -0600

    reverting

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename='https://chapachris16.github.io/portfolio2'>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
